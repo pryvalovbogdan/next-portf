f9a049d99ca921d16c0d83c5da7d15835723d09d
bogdanpryvalov <pryvalov.bogddan@gmail.com>
2024-07-13 18:40:40 +0300

    added first variant of realization

---
+++ src/app/[lng]/client-page/layout.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from '../../i18n';
+import { fallbackLng, languages } from '../../i18n/settings';
+
+export async function generateStaticParams() {
+  return languages.map(lng => ({ lng }));
+}
+
+export async function generateMetadata({ params: { lng } }) {
+  if (languages.indexOf(lng) < 0) lng = fallbackLng;
+
+  const { t } = await useTranslation(lng, 'client-page');
+
+  return {
+    title: t('title'),
+  };
+}
+
+export default async function Layout({ children }) {
+  return children;
+}
---
+++ src/app/[lng]/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import { useTranslation } from 'src/app/i18n';
+import { FooterBase } from './FooterBase';
+
+export const Footer = async ({ lng, path }) => {
+  const { t } = await useTranslation(lng, 'footer');
+
+  return <FooterBase t={t} lng={lng} path={path} />;
+};
