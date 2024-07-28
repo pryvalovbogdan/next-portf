f58ffb867f43ec570a09addfa66c0b5f6530c004
bogdanpryvalov <pryvalov.bogddan@gmail.com>
2024-07-28 21:08:17 +0300

    removed dashboard

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,11 +1,10 @@
 import { Advent_Pro } from 'next/font/google';
 
-import Dashboard from 'src/app/components/Dashboard/Dashboard';
+import { Footer } from 'src/app/components/Footer/client';
+import { ModelWrapper } from 'src/app/components/Model/CanvasWrapper';
 import { NavBar } from 'src/app/components/Navbar/NavBar';
 
 import { fallbackLng, languages } from '../i18n/settings';
-import { Footer } from 'src/app/components/Footer/client';
-import { ModelWrapper } from 'src/app/components/Model/CanvasWrapper';
 
 // If loading a variable font, you don't need to specify the font weight
 const font = Advent_Pro({ subsets: ['latin'], variable: '--font-advent-pro' });
@@ -27,7 +26,6 @@ export default async function Page({
         </main>
         <NavBar lng={lng} />
         <div className='snap-always snap-center'>
-          <Dashboard />
           <Footer lng={lng} path={''} />
         </div>
       </div>
